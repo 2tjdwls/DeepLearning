f5de0703f518aec0efe81deedccde3e46f2d4875
LeeSeongJin <tjdwls3470@naver.com>
2022-01-11 19:36:53 +0900

    Coursera_Lecture1_Assignment_w2

--- Coursera_Lecture1_Assignment_w2.py
+++ Coursera_Lecture1_Assignment_w2.py
@@ -8,10 +8,6 @@ from scipy import ndimage
 from lr_utils import load_dataset
 from public_tests import *
 
-%matplotlib inline
-%load_ext autoreload
-%autoreload 2
-
 # Loading the data (cat/non-cat)
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
